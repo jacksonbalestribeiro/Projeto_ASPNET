867594315a3133d42fdfe73e5404455d8449fa35
Jackson Balest <balestjackson@gmail.com>
2022-04-23 19:54:00 -0300

    Migrando outras entidades

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -9,5 +9,24 @@ namespace SalesWebMvc.Models
     {
         public int id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        public Department()
+        {
+        }
+        public Department(int id, string name)
+        {
+            this.id = id;
+            Name = name;
+        }
+        public void addSeller(Seller sr)
+        {
+            Sellers.Add(sr);
+        }
+
+        public double totalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.totalSales(initial, final));
+        }
     }
 }
