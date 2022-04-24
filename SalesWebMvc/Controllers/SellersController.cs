6e95e5b6e2ef55a814fe566acf82f563da61710e
Jackson Balest <balestjackson@gmail.com>
2022-04-24 20:28:38 -0300

    detalhando a view Sellers

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -3,14 +3,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using SalesWebMvc.Services;
 
 namespace SalesWebMvc.Controllers
 {
     public class SellersController : Controller
     {
+        private readonly SellerServices _sellerServices;
+
+        public SellersController(SellerServices sellerServices)
+        {
+            _sellerServices = sellerServices;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var list = _sellerServices.findAll();
+            return View(list);
         }
     }
 }
