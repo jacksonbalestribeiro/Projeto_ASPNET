3dbe041ad1f07c3aa80a0f66af239e75c84598b7
Jackson Balest <balestjackson@gmail.com>
2022-04-26 21:50:42 -0300

    Form de Sellers

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using SalesWebMvc.Services;
+using SalesWebMvc.Models;
 
 namespace SalesWebMvc.Controllers
 {
@@ -20,6 +21,19 @@ namespace SalesWebMvc.Controllers
         {
             var list = _sellerServices.findAll();
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerServices.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
