0369cbe05d316e9f83e71518d1b0115bba8274ee
Jackson Balest <balestjackson@gmail.com>
2022-04-29 22:31:24 -0300

    Foreing Sellers

---
+++ SalesWebMvc/Migrations/20220430011746_DepartmentForeignKey.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SalesWebMvc.Migrations
+{
+    public partial class DepartmentForeignKey : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_salesRecord_Seller_SellerId",
+                table: "salesRecord");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Seller_Department_Departmentid",
+                table: "Seller");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_salesRecord",
+                table: "salesRecord");
+
+            migrationBuilder.RenameTable(
+                name: "salesRecord",
+                newName: "SalesRecord");
+
+            migrationBuilder.RenameColumn(
+                name: "Departmentid",
+                table: "Seller",
+                newName: "DepartmentId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Seller_Departmentid",
+                table: "Seller",
+                newName: "IX_Seller_DepartmentId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_salesRecord_SellerId",
+                table: "SalesRecord",
+                newName: "IX_SalesRecord_SellerId");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DepartmentId",
+                table: "Seller",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_SalesRecord",
+                table: "SalesRecord",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SalesRecord_Seller_SellerId",
+                table: "SalesRecord",
+                column: "SellerId",
+                principalTable: "Seller",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Seller_Department_DepartmentId",
+                table: "Seller",
+                column: "DepartmentId",
+                principalTable: "Department",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SalesRecord_Seller_SellerId",
+                table: "SalesRecord");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Seller_Department_DepartmentId",
+                table: "Seller");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_SalesRecord",
+                table: "SalesRecord");
+
+            migrationBuilder.RenameTable(
+                name: "SalesRecord",
+                newName: "salesRecord");
+
+            migrationBuilder.RenameColumn(
+                name: "DepartmentId",
+                table: "Seller",
+                newName: "Departmentid");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Seller_DepartmentId",
+                table: "Seller",
+                newName: "IX_Seller_Departmentid");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_SalesRecord_SellerId",
+                table: "salesRecord",
+                newName: "IX_salesRecord_SellerId");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Departmentid",
+                table: "Seller",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_salesRecord",
+                table: "salesRecord",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_salesRecord_Seller_SellerId",
+                table: "salesRecord",
+                column: "SellerId",
+                principalTable: "Seller",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Seller_Department_Departmentid",
+                table: "Seller",
+                column: "Departmentid",
+                principalTable: "Department",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
