2117ce47a64d4ec5fa1d3e9a36c88d6130b5355c
Jackson Balest <balestjackson@gmail.com>
2022-05-14 17:43:06 -0300

    criando relatorios

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -46,7 +46,7 @@ namespace SalesWebMvc
             services.AddScoped<SeedingService>();
             services.AddScoped<SellerServices>();
             services.AddScoped<DepartmentService>();
-
+            services.AddScoped<SalesRecordService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
